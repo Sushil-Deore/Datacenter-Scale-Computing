fa93ec0c59b6f8ae47f0b18526cba1e241d993cc
Sushil Deore <sude8287@MacBook-Pro.local>
2023-10-01 17:28:20 -0600

    adding compose and updates

--- Lab-2/pipeline.py
+++ Lab-2/pipeline.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import argparse
+from sqlalchemy import create_engine
 
 def extract_data(source):
      
@@ -25,18 +27,23 @@ def transform_data(data):
 
     return df
 
-def load_data(data, target):
+def load_data(data):
     # Exporting dataframe to csv file
-    data.to_csv(target)
+    db_url = "postgresql+psycopg2://sushil:hunter2@db:5432/shelter"
+    conn = create_engine(db_url)
+    data.to_sql("outcomes", conn, if_exists= "append", index=False)
 
 if __name__ == "__main__":
-
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('source', help='source csv')
+    #parser.add_argument('target', help='target csv')
     source= "https://data.austintexas.gov/api/views/9t4d-g238/rows.csv"
-    target = "Processed.csv"
+    #target = "Processed.csv"
+    #args = parser.parse_args()
 
     print("Starting...")
     df = extract_data(source)
     new_df = transform_data(df)
-    load_data(new_df, target=target)
+    load_data(new_df)
     
     print("Complete")
